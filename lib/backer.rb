68120dafff0215b38f256a2f4b0aa08ce73f9411
Alicia Santiago <albrown07@mac.com>
2019-02-27 16:45:53 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Backer
    attr_reader :name, :backed_projects
   
@@ -10,7 +11,7 @@ class Backer
   def back_project(project)
     @backed_projects << Project.new
   end
-  
+  binding.pry
   
   
   
